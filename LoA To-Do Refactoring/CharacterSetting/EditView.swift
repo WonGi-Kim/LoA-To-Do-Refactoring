54e614df571f8afd5c840dbbba54d5172925014f
WonGi-Kim <kwg970310@gmail.com>
2023-11-02 15:46:32 +0900

    EditView 생성후 데이터 전달 확인 완료

---
+++ LoA To-Do Refactoring/CharacterSetting/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  LoA To-Do Refactoring
+//
+//  Created by 김원기 on 10/30/23.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
